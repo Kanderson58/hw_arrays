b24e0b9985c4fd9b0cf6e8c8cacd185d15965531
Kara Anderson <kanderson8585@gmail.com>
2022-10-18 13:03:32 -0600

    Demonstrate understanding of index positions

--- arrays.js
+++ arrays.js
@@ -15,3 +15,8 @@ console.log(petsAge);
 // true in the 1 spot, and false in the 2 spot.
 boolean.shift();
 console.log(boolean);
+
+// Index positions start from 0 as the first place and go up from there.  It is a way to refer
+// to where an item is within an array.
+var petsAge = [1, 6, 13, 3];
+// In this array, my cat Rosie's age, 13, has the index position 2.
