42fc146c69a6fc472e98e36fb08d17ee1696b148
Kara Anderson <kanderson8585@gmail.com>
2022-10-18 13:00:44 -0600

    Show three different array methods

--- arrays.js
+++ arrays.js
@@ -1,3 +1,17 @@
-var favoriteFood = ["raspberries", "potstickers", "tamales", "garlic bread"]
-var petsAge = [1, 6, 13, 3]
-var boolean = [true, false, true, false]
+var favoriteFood = ["raspberries", "potstickers", "tamales", "garlic bread"];
+var petsAge = [1, 6, 13, 3];
+var boolean = [true, false, true, false];
+
+// Using .pop will remove the last string in this array and it will output the array 
+// minus the final string, "garlic bread".
+favoriteFood.pop();
+console.log(favoriteFood);
+
+// Using .push will add a number, in this case 5, to the end of the array.
+petsAge.push(5);
+console.log(petsAge);
+
+// Using .shift will remove the first boolean from the array, leaving false in the 0 spot,
+// true in the 1 spot, and false in the 2 spot.
+boolean.shift();
+console.log(boolean);
